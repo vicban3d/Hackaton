f37ec4a7e850aaf6d22fc8b0d43f10f95277d199
victorb <victorb@checkpoint.com>
2015-10-28 14:21:04 +0200

    Added GUI

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/GUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class GUI : Form
+    {
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        void drawData()
+        {
+            Label l = new Label();
+            l.Text = "some field";
+            l.Size = new System.Drawing.Size(166, 83);
+            p_view.Controls.Add(l);
+
+        }
+
+        private void b_show_Click(object sender, EventArgs e)
+        {
+            drawData();
+        }
+    }
+
+   
+
+
+
+}
